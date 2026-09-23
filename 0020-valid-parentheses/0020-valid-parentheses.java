4e00df24ba6fcbbcf47e80206ad92a9464029574
Ansh Patel <anshpatel8299@gmail.com>
2026-09-23 21:57:30 +0530

    Time: 3 ms (86.14%), Space: 43.4 MB (40.97%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -12,18 +12,11 @@ class Solution {
 
                 char top = stack.pop();
 
-                if (ch == ')' && top != '(') {
+                if (ch == ')' && top != '('|| ch==']' && top!='[' || ch=='}' && top!='{') {
                     return false;
-                }
-
-                if (ch == '}' && top != '{') {
-                    return false;
-                }
-
-                if (ch == ']' && top != '[') {
-                    return false;
-                }
-            }           
+               
+            }   
+            }        
 
       }
       return stack.isEmpty();
